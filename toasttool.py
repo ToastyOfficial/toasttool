c99399d788b747d6d5dc9098e6c97e17a5aa8d18
ToastyOfficial <72546188+ToastyOfficial@users.noreply.github.com>
2023-09-28 08:44:15 -0400

    Update toasttool.py

--- toasttool.py
+++ toasttool.py
@@ -10,23 +10,7 @@ from time import sleep
 
 from core import FingerOfGodCollection
 from tools.anonsurf import AnonSurfTools
-from tools.ddos import DDOSTools
-from tools.exploit_frameworks import ExploitFrameworkTools
-from tools.forensic_tools import ForensicTools
-from tools.information_gathering_tools import InformationGatheringTools
-from tools.other_tools import OtherTools
-from tools.payload_creator import PayloadCreatorTools
-from tools.phising_attack import PhishingAttackTools
-from tools.post_exploitation import PostExploitationTools
-from tools.remote_administration import RemoteAdministrationTools
-from tools.reverse_engineering import ReverseEngineeringTools
-from tools.sql_tools import SqlInjectionTools
-from tools.steganography import SteganographyTools
 from tools.tool_manager import ToolManager
-from tools.webattack import WebAttackTools
-from tools.wireless_attack_tools import WirelessAttackTools
-from tools.wordlist_generator import WordlistGeneratorTools
-from tools.xss_attack import XSSAttackTools
 
 logo = """\033[33m
  ______   ______     ______     ______     ______      ______   ______     ______     __       
